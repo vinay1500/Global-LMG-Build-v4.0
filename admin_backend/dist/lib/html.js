ab24f1e306ce11aaf08df613f7c1a0b85514bcf9
Vinay <your-email@example.com>
2026-04-06 18:24:23 +0530

    Stop tracking ignored files

--- admin_backend/dist/lib/html.js
+++
@@ -1,7 +0,0 @@
-export const escapeHtml = (value) => value
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
-export const textToHtmlParagraph = (value) => `<p>${escapeHtml(value).replace(/\n/g, '<br />')}</p>`;
--- backend/dist/modules/clientAccounts/types.js
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_NOTIFICATION_PREFERENCES = {
-    caseActivityAlerts: true,
-    emailUpdates: true,
-    invoiceReminders: true,
-    productAnnouncements: false,
-    smsAlerts: true,
-};
